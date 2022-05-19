75913b5f99df303f014045aafb6994fba2b280b7
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-19 02:02:55 -0700

    add comment formHandler

---
+++ public/js/comment.js
@@ -0,0 +1,23 @@
+const newFormHandler = async (event) => {
+  const content = document.querySelector('#comment-content').value.trim();
+  const post_id = event.target.getAttribute('data-post-id');
+
+  if (content && post_id) {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({ content, post_id }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace(`/post/${post_id}/comment`);
+      console.log('Comment created')
+    } else {
+      alert('Failed to create post');
+    }
+  }
+};
+
+document.querySelector('#create-comment').addEventListener('click', newFormHandler);
