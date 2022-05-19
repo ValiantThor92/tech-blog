bc13ea7a3890bd6e6dda81c25a4c7c1ef9422375
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-19 01:15:02 -0700

    add event handlers to post, add hndlbrs for post

---
+++ public/javascript/post.js
@@ -0,0 +1,42 @@
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+const updateHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+
+    if (title && content) {
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to update post');
+      }
+    }
+  }
+}
+
+document.querySelector('#delete-post').addEventListener('click', delButtonHandler);
+document.querySelector('#update-post').addEventListener('click', updateHandler);
