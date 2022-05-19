2685c93569bece8659532e9838b9832973ff4e7b
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-19 10:10:04 -0700

    add get route for single post by id

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,18 +5,58 @@ const withAuth = require('../utils/auth');
 // get all posts
 router.get('/', async (req, res) => {
   try {
-      const postData = await Post.findAll({
-          include: [{ model: User, attributes: ['first_name', 'last_name']}, { model: Comment, attributes: ['content', 'date_created'], as: 'comments' }]
-      })
-      const posts = postData.map(post => post.get({ plain: true }));
-      console.log(posts);
-      res.render('homepage', { posts, logged_in: req.session.logged_in });
+    const postData = await Post.findAll({
+      include: [
+        { 
+          model: User, 
+          attributes: ['first_name', 'last_name']
+        },
+        { model: Comment, 
+          attributes: ['content', 'date_created'],
+          as: 'comments' 
+        }
+      ]
+    })
+    const posts = postData.map(post => post.get(
+      {
+        plain: true
+      }));
+    console.log(posts);
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
       res.status(500).json(err);
   }
 });
 // get single post by id
-router.get('/post/:id', withAuth, async (req, res) => {})
+router.get('/post/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        { 
+          model: User,
+          attributes: ['first_name', 'last_name']
+        }
+      ]
+    })
+    const post = postData.get(
+      {
+        plain: true
+      }
+    );
+    console.log(post);
+    res.render('post',
+      {
+        ...post,
+        logged_in: req.session.logged_in
+      }
+    )
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 // get single post comments
 router.get('/post/:id/comment', withAuth, async (req, res) => {})
 // get dashboard data
