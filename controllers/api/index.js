a878dbdf23f27ea05f729f3eb68544d288ad9832
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-06-03 13:24:26 -0700

    debug and polish

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const userRouter = require('./userRoutes');
-const postRouter = require('./postRoutes');
-const commentRouter = require('./commentRoutes');
+const userRouter = require('./user-routes');
+const postRouter = require('./post-routes');
+const commentRouter = require('./comment-routes');
 
 router.use('/users', userRouter);
 router.use('/posts', postRouter);
